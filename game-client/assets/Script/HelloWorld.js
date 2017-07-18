337ccdb4f381823fd8063f4fbb32d5194d863aed
林艺滨 <linyibin@xunlegame.com>
2017-07-19 00:23:19 +0800

    init

--- game-client/assets/Script/HelloWorld.js
+++ game-client/assets/Script/HelloWorld.js
@@ -1,3 +1,4 @@
+//var config_NetRoute = require("config_NetRoute");
 cc.Class({
     extends: cc.Component,
 
@@ -29,20 +30,34 @@ cc.Class({
             port: self.serverPort,
             log: true
         }, function(data) {
-            pomelo.request("connector.entryHandler.entry", {
-                uid: "1"
-            }, function(data) {
-                cc.log("pomelo.request return data: ", data);
-
+            pomelo.request("connector.entryHandler.entry", {account:"001", password:"123456"}, function (msg) {
+                console.log(msg);
                 pomelo.disconnect();
-                if(data.code === 500) {
+                if (msg.code == 500) {
+                    console.log("服务器连接失败");
                     return;
                 }
-                // 返回的data.host为"127.0.0.1"，
-                // 			测试时需要改成本机ip否则Android手机连接不上
-                // 			callback(data.host, data.port);
-                //callback(that.serverIp, data.port);
-                cc.log("200")
+                pomelo.init({
+                    host: msg.host,
+                    port: msg.port,
+                    log: true
+                }, function (msg) {
+                    pomelo.request("connector.entryHandler.entry", {
+                        uid: "1"
+                    }, function(data) {
+                        cc.log("pomelo.request return data: ", data);
+
+                        pomelo.disconnect();
+                        if(data.code === 500) {
+                            return;
+                        }
+                        // 返回的data.host为"127.0.0.1"，
+                        // 			测试时需要改成本机ip否则Android手机连接不上
+                        // 			callback(data.host, data.port);
+                        //callback(that.serverIp, data.port);
+                        cc.log("200")
+                    });
+                });
             });
         });
     }
